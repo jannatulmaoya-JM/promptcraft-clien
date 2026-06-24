e1f150032885f5c8c35e2e816a4c9e3f2c49b59a
maoya12345 <jannatulmaoya456@gmail.com>
2026-06-24 15:43:04 +0600

    api route file

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -4,3 +4,11 @@
 //   baseURL: process.env.BETTER_AUTH_URL,
 //   plugins: [jwtClient()],
 // });
+
+import { createAuthClient } from "better-auth/client";
+import { jwtClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
+export const authClient = createAuthClient({
+  baseURL: process.env.BETTER_AUTH_URL,
+  plugins: [jwtClient()],
+});
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,36 +1,44 @@
-// import dns from "node:dns";
-// dns.setServers(["1.1.1.1", "1.0.0.1"]);
-// import { betterAuth } from "better-auth";
-// import { MongoClient } from "mongodb";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-// import { jwt } from "better-auth/plugins";
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
+import { client } from "@/db";
 
-// const client = new MongoClient(process.env.MONGODB_URI);
-// const db = client.db("tech-bazaar");
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("tech-bazaar");
 
-// export const auth = betterAuth({
-//   database: mongodbAdapter(db, {
-//     client,
-//   }),
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-//   user: {
-//     additionalFields: {
-//       role: {
-//         defaultValue: "buyer",
-//       },
-//       plan: {
-//         defaultValue: "free",
-//       },
-//     },
-//   },
-//   session: {
-//     cookieCache: {
-//       enabled: true,
-//       strategy: "jwt",
-//       maxAge: 60 * 24 * 60,
-//     },
-//   },
-//   plugins: [jwt()],
-// });
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+   baseURL: process.env.BETTER_AUTH_URL, 
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID , 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET , 
+        }, 
+    },
+  user: {
+    additionalFields: {
+      role: {
+        defaultValue: "buyer",
+      },
+      plan: {
+        defaultValue: "free",
+      },
+    },
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      strategy: "jwt",
+      maxAge: 60 * 24 * 60,
+    },
+  },
+  plugins: [jwt()],
+});
